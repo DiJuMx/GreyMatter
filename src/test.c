bf39d5dc15a3ae32a8962185e75637f1eb2e8916
Daniel Moran <dijumx@gmail.com>
2019-05-05 21:25:06 +0100

    Add preliminary perceptron test code

--- src/test.c
+++ src/test.c
@@ -1,12 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+#include "GreyMatter/unit.h"
+#include "GreyMatter/perceptron.h"
 
 #define UNUSED(x) ((void)(x))
+
+float tanh_deriv(float x)
+{
+        return 1.f/(coshf(x)*coshf(x));
+}
 
 int main(int argc, char*argv[])
 {
         UNUSED(argc);
         UNUSED(argv);
-        printf("TEST RUN\n");
+        int status = 0; 
+        struct gm_unit * layer1 = gmCreateUnit(2, 1);
+        if (NULL == layer1){
+                printf("Error creating unit\n");
+                return EXIT_FAILURE;
+        }
+
+        status = gmCreatePerceptron(layer1, tanhf, tanh_deriv);
+        if (-1 == status){
+                printf("Error creating Perceptron\n");
+                return EXIT_FAILURE;
+        }
+
+        
+
         return EXIT_SUCCESS;
 }
